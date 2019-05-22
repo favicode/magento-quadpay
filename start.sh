8dbafca02dc5b4eee0215f2204e87466678a017a
Domagoj Potkoc <dpotkoc@gmail.com>
2019-05-22 14:30:05 +0200

    inital commit

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Handle the IP change
+cat /etc/hosts | grep -v $MAGENTO_HOST > /etc/hosts
+echo "`/sbin/ip route|awk '/default/ { print $3 }' | grep -v ppp` "$MAGENTO_HOST | tee -a /etc/hosts > /dev/null
+
+# Setup Apache document root
+sed "s#DOCUMENT_ROOT#${DOCUMENT_ROOT:-/var/www/html}#g" -i /etc/apache2/sites-available/000-default.conf
+
+# Run services
+service ssh start
+nohup /usr/sbin/php-fpm7.2 &
+/usr/sbin/apache2ctl -D FOREGROUND
