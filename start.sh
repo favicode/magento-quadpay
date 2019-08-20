1cdfb2baa3a7d2aa93354945b1f99a68e25d8b8f
Domagoj Potkoc <dpotkoc@gmail.com>
2019-08-20 09:39:33 +0200

    install script

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Handle the IP change
-cat /etc/hosts | grep -v $MAGENTO_HOST > /etc/hosts
-echo "`/sbin/ip route|awk '/default/ { print $3 }' | grep -v ppp` "$MAGENTO_HOST | tee -a /etc/hosts > /dev/null
+#cat /etc/hosts | grep -v $MAGENTO_HOST > /etc/hosts
+#echo "`/sbin/ip route|awk '/default/ { print $3 }' | grep -v ppp` "$MAGENTO_HOST | tee -a /etc/hosts > /dev/null
 
 # Setup Apache document root
 sed "s#DOCUMENT_ROOT#${DOCUMENT_ROOT:-/var/www/html}#g" -i /etc/apache2/sites-available/000-default.conf
